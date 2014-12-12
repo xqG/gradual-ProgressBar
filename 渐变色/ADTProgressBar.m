dd09342d9178a7923445665c24f210a306b89043
高新强 <xq910612@163.com>
2014-12-12 21:55:54 +0800

    first commit

---
+++ 渐变色/ADTProgressBar.m
@@ -0,0 +1,21 @@
+//
+//  ADTProgressBar.m
+//  渐变色
+//
+//  Created by 高新强 on 14-10-12.
+//  Copyright (c) 2014年 Gxq. All rights reserved.
+//
+
+#import "ADTProgressBar.h"
+
+@implementation ADTProgressBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
